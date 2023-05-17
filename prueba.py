b00311b7fefced4f956e53d620cc0c0392f82744
AdrianaGarcia31 <ag86879@email.vccs.edu>
2023-05-17 08:00:11 -0400

    valid func

--- prueba.py
+++ prueba.py
@@ -10,15 +10,14 @@ board = [
         [0, 4, 9, 2, 0, 6, 0, 0, 7]
     ]
 
-def print_board(bo):
-    for i in range(len(board)):
-        if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - -")
+
+#def print_board(bo):
+for i in range(len(board)):
+    if i % 3 == 0 and i != 0:
+        print("- - - - - - - - - -")
     for j in range(len(board[0])):
         if j % 3 == 0 and j != 0:
             print(' | ', end='')
         if j == 8:
             print()
 
-def valid():
-    print()
