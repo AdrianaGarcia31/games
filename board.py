357201e682d3379130bf7b56321d95c017a7bc90
AdrianaGarcia31 <ag86879@email.vccs.edu>
2023-05-18 11:07:15 -0400

    3th

--- board.py
+++ board.py
@@ -10,22 +10,36 @@ board = [
         [0, 4, 9, 2, 0, 6, 0, 0, 7]
     ]
 
+def solve(bo):
+    empty = find_empty(bo)
+    if not empty :
+        return True
+    else:
+        row, col = empty
+    for i in range(1,10):
+            valida = valid(bo, i , empty)
+
+                
+
 def print_board(bo):
-    for i in range(len(board)):
+    for i in range(len(bo)):
         if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - -")
-    for j in range(len(board[0])):
-        if j % 3 == 0 and j != 0:
-            print(' | ', end='')
-        if j == 8:
-            print()
+            print("- - - - - - - - - - - -")
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(' | ', end='')
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(bo[i][j], end=' ')
+
 
 def find_empty(bo):
     for i in range(len(bo)):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:
                 return(i, j) # row, col (tuple)
-
+    return None
 
 def valid(bo, num, pos):
 
@@ -40,4 +54,14 @@ def valid(bo, num, pos):
             return False
         
     #check square
+    box_x = pos[0] // 3
+    box_y = pos[1] // 3
+
+    for i in range(box_x*3,box_x*3+3):
+        for j in range(box_y*3, box_y*3+3):
+            if bo[i][j] == num and pos != (i, j):
+                return False
     
+    return True
+
+print_board(board)
